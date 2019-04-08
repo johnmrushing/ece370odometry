a70c682f7e2f51ed93688bd33141bed1f5b01c55
johnmrushing <johnmrushing@gmail.com>
2019-04-07 23:01:54 -0400

    Update matrix.ino

--- matrix.ino
+++ matrix.ino
@@ -15,6 +15,34 @@ float thetaZ = 0, _thetaZ = tickDistance / baseline;
 float x = 0, _x = ( baseline / 2 ) * sin ( _thetaZ ), _x0;
 float y = 0, _y = ( baseline / 2 ) * ( 1 - cos ( _thetaZ ) ), _y0;
 
+Matrix<4,4> rotationR    = {  cos( _thetaZ ), -sin( _thetaZ ),  0,  0,
+                              sin( _thetaZ ),  cos( _thetaZ ),  0,  baseline / 2,
+                              0,  0,  1,  0,
+                              0,  0,  0,  1};
+
+Matrix<4,4> rotationR    = {  cos( -_thetaZ ), -sin( -_thetaZ ),  0,  0,
+                              sin( -_thetaZ ),  cos( -_thetaZ ),  0,  -baseline / 2,
+                              0,  0,  1,  0,
+                              0,  0,  0,  1};
+
+Matrix<4,4> translationR = {  1,  0,  0,  0,
+                              0,  1,  0,  -baseline,
+                              0,  0,  1,  0,
+                              0,  0,  0,  1};
+
+Matrix<4,4> translationL = {  1,  0,  0,  0,
+                              0,  1,  0,  baseline,
+                              0,  0,  1,  0,
+                              0,  0,  0,  1};
+
+Matrix<4,4> transformR = rotationR * translationR;
+Matrix<4,4> transformL = rotationL * translationL;
+
+Matrix<4,4> globalMatrix = {  1,  0,  0,  0,
+                              0,  1,  0,  0,
+                              0,  0,  1,  0,
+                              0,  0,  0,  1};
+
 void rightTickInterrupt()
 { 
   globalMatrix = globalMatrix * transformR;
@@ -37,34 +65,6 @@ void setup()
 {
 
   Serial.begin(9600); // arduino IDE
-
-  Matrix<4,4> rotationR    = {  cos( _thetaZ ), -sin( _thetaZ ),  0,  0,
-                                sin( _thetaZ ),  cos( _thetaZ ),  0,  baseline / 2,
-                                0,  0,  1,  0,
-                                0,  0,  0,  1};
-
-  Matrix<4,4> rotationR    = {  cos( -_thetaZ ), -sin( -_thetaZ ),  0,  0,
-                                sin( -_thetaZ ),  cos( -_thetaZ ),  0,  -baseline / 2,
-                                0,  0,  1,  0,
-                                0,  0,  0,  1};
-                                    
-  Matrix<4,4> translationR = {  1,  0,  0,  0,
-                                0,  1,  0,  -baseline,
-                                0,  0,  1,  0,
-                                0,  0,  0,  1};
-
-  Matrix<4,4> translationL = {  1,  0,  0,  0,
-                                0,  1,  0,  baseline,
-                                0,  0,  1,  0,
-                                0,  0,  0,  1};
-  
-  Matrix<4,4> transformR = rotationR * translationR;
-  Matrix<4,4> transformL = rotationL * translationL;
-
-  Matrix<4,4> globalMatrix = {  1,  0,  0,  0,
-                                0,  1,  0,  0,
-                                0,  0,  1,  0,
-                                0,  0,  0,  1};
   
   pinMode(LEFTIR, INPUT_PULLUP);
   pinMode(RIGHTIR, INPUT_PULLUP);
